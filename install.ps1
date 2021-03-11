6e2647e0060ff175748f4282bd9c3c082d9e752f
enscogitans <37674855+enscogitans@users.noreply.github.com>
2021-03-12 02:05:02 +0300

    Add install script

---
+++ install.ps1
@@ -0,0 +1,9 @@
+param(
+    [string]$dst_dir="bin",
+    [string]$build_dir="build"
+)
+
+mkdir $build_dir -ErrorAction SilentlyContinue
+cmake -S . -B $build_dir -DCMAKE_BUILD_TYPE=Release
+cmake --build $build_dir --config Release
+cmake --install $build_dir --prefix $dst_dir --config Release
